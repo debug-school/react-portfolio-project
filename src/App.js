d69a9291d475e435d6a71a2ca40faed4c46d39fa
Rasel Rana <debug.school404@gmail.com>
2021-03-29 05:09:28 -0500

    Menu Bar

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import "bootstrap/dist/css/bootstrap.min.css";
 import './App.css';
+import NavBar from "./components/NavBar.js";
 
 function App() {
   return (
     <div className="App">
-      <h1></h1>
+      <NavBar />
     </div>
   );
 }
